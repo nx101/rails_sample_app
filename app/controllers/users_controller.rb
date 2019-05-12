cff86b82569807bdf9e089b6a79f55852802b61c
nx101 <revelight@gmail.com>
2019-05-12 11:14:12 +0300

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,36 @@
 class UsersController < ApplicationController
-  def new
+
+  def show
+    # we create this instance var to be used by the 'show' view
+    @user = User.find(params[:id])
+    # debugger
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # new user saved - Handle a successful save
+      # assign a msg to rails flash msg hash
+      flash[:success] = "Hello newly created user, Welcome to the Sample App!"
+      # redirect to the new user's profile
+      redirect_to @user  # same as: redirect_to user_url(@user)
+    else
+      render 'new'
+    end
+  end
+
+
+  private
+  # returns a filtered version of params
+  # require -- raises error if a :user attr missing
+  # permit -- ret only the specified attrs
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+
 end
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,53 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name:  "",
+                                         email: "user@invalid",
+                                         password:              "foo",
+                                         password_confirmation: "bar" } }
+    end
+    # check that a failed submission re-renders the new action
+    assert_template 'users/new'
+
+    # tests for error msgs
+    # by css id for error explanation
+    assert_select 'div#error_explanation'
+    # CSS class for fields with error
+    assert_select 'div.field_with_errors'
+
+    # test correct post path for form
+    # by presence of the form tag
+    assert_select 'form[action="/signup"]'
+
+  end
+
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+    end
+
+    # follow the redirect_to in the code and test the template
+    follow_redirect!
+    assert_template 'users/show'
+    # test for a non-empty flash msg (ow brittle)
+    assert_not flash.empty?
+
+  end
+
+
+
+
+
+
+
+
+end
